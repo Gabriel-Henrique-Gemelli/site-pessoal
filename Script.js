8ecc90f365c5b3dbddf118152eac746459693bcd
ximbas2508 <126275774+ximbas2508@users.noreply.github.com>
2023-11-20 17:46:19 -0300

    site pessoal 3.0

--- Script.js
+++ Script.js
@@ -1,5 +1,23 @@
 function copy(){
-    navigator.clipboard.writeText("gabrielhenriquegemelli@gmail.com").then((
-        alert("Email copiado: gabrielhenriquegemelli@gmail.com ")
-    ))
+    navigator.clipboard.writeText("gabrielhenriquegemelli@gmail.com")
+}
+
+
+
+function exibirAlerta() {
+    
+    copy()
+    document.getElementById('alerta').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+    document.body.style.overflow = "hidden"
+    
+  }
+
+
+function fecharAlerta() {
+
+    document.getElementById('alerta').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+    document.body.style.overflow = "auto"
+
 }
